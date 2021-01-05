f61e33821fc71c2280e002ccf737be9133e47c7e
ricardorojass <reddrum7@gmail.com>
2021-01-05 13:40:40 -0500

    initial setup

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = () => (
+  <h1>My Survey System with Redux</h1>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+)
